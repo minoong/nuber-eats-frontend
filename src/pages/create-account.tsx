07459f796a8760d1cdfe61458878f558ddb49100
lmw <alsdndjdrktl@gmail.com>
2021-09-25 20:25:33 +0900

    common tailwind css and login form

---
+++ src/pages/create-account.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CreateAccount = () => {
+ return <div>createAccount</div>
+}
+
+export default CreateAccount
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,57 +1,19 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
-import { isLoggedInVar } from '../apollo'
-
-interface IForm {
- email: string
- password: string
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import CreateAccount from '../pages/create-account'
+import Login from '../pages/login'
 
 export const LoggedOutRouter = () => {
- const {
-  register,
-  handleSubmit,
-  watch,
-  formState: { errors },
- } = useForm<IForm>()
-
- const onSubmit = () => {
-  console.log(watch())
- }
-
- const onInvalid = () => {
-  console.log(`can't create account.`)
- }
-
  return (
-  <div>
-   <h1>Logged Out</h1>
-   <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-    <div>
-     <input
-      {...register('email', {
-       required: 'This is required',
-       pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-      })}
-      type="email"
-      name="email"
-      //   required
-      placeholder="email"
-     />
-     {errors.email?.message && <span className="font-bold text-red-600">{errors.email.message}</span>}
-     {errors.email?.type === 'pattern' && <span className="font-bold text-red-600">Only gmail allowed</span>}
-    </div>
-    <div>
-     <input
-      {...register('password', { required: true })}
-      type="password"
-      name="password"
-      required
-      placeholder="password"
-     />
-    </div>
-    <button className="bg-yellow-300 text-white">Submit</button>
-   </form>
-  </div>
+  <Router>
+   <Switch>
+    <Route path="/create-account">
+     <CreateAccount />
+    </Route>
+    <Route path="/">
+     <Login />
+    </Route>
+   </Switch>
+  </Router>
  )
 }
