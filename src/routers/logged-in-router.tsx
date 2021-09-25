3f40f631664541fd2382a24ca1477d0495360cb9
lmw <alsdndjdrktl@gmail.com>
2021-09-25 19:09:39 +0900

    apply react hook form

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,2 +1,8 @@
 import React from 'react'
-export const LoggedInRouter = () => <span>Logged In</span>
+import { isLoggedInVar } from '../apollo'
+export const LoggedInRouter = () => (
+ <div>
+  <h1>Logged In</h1>
+  <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+ </div>
+)
