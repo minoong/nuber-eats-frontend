95ca3df953ede35aa155c82a0fa4da4654e906f6
lmw <alsdndjdrktl@gmail.com>
2022-03-27 18:28:24 +0900

    feat : readme updated

--- README.md
+++ README.md
@@ -1,46 +1,24 @@
-# Getting Started with Create React App
+# Nuber Eats BackEnd
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+---
 
-## Available Scripts
+1. 프론트엔드: ReactJS. GraphQL. Typescript. Apollo. TailwindCSS
+2. 백엔드: NestJS, Typescript, TypeORM, GraphQL
+3. E2E Testing + Unit Testing (백엔드: Jest. Supertest | 프론트엔드: Jest. React Testing Library, Cypress)
+4. 데이터 시각화 (Victory)
+5. 100% 리얼타임. Naver Maps 연동
+6. 3개의 클라이언트 (유저+레스토랑+딜리버리)
 
-In the project directory, you can run:
+---
 
-### `npm start`
+#### 1. 로그인
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+![음식점로그인](https://user-images.githubusercontent.com/59505318/160275111-c81b5c1d-c4bd-42f9-9975-c2c301a0d5d6.gif)
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+#### 2. 레스토랑 등록
 
-### `npm test`
+![등록](https://user-images.githubusercontent.com/59505318/160275296-e53bd7f3-4e87-4fc4-b7bf-7d1213526fde.gif)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+#### 3. 음식 주문 3개 클라이언트 연동 푸시
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+![연동](https://user-images.githubusercontent.com/59505318/160275313-dd288a18-c986-44fc-8354-c176be3b91d2.gif)
--- src/pages/user/order.tsx
+++ src/pages/user/order.tsx
@@ -41,117 +41,119 @@ const EDIT_ORDER = gql`
 `
 
 interface IParams {
- id: string
+  id: string
 }
 
 const Order = () => {
- const params = useParams<IParams>()
- const { data: userData } = useMe()
- const [editOrderMutation] = useMutation<editOrder, editOrderVariables>(EDIT_ORDER)
- const { data, subscribeToMore } = useQuery<getOrder, getOrderVariables>(GET_ORDER, {
-  variables: {
-   input: {
-    id: +params.id,
-   },
-  },
- })
-
- useEffect(() => {
-  if (data?.getOrder.ok) {
-   subscribeToMore({
-    document: ORDER_SUBSCRIPTION,
+  const params = useParams<IParams>()
+  const { data: userData } = useMe()
+  const [editOrderMutation] = useMutation<editOrder, editOrderVariables>(EDIT_ORDER)
+  const { data, subscribeToMore } = useQuery<getOrder, getOrderVariables>(GET_ORDER, {
     variables: {
-     input: {
-      id: +params.id,
-     },
-    },
-    updateQuery: (prev, { subscriptionData: { data } }: { subscriptionData: { data: orderUpdates } }) => {
-     if (!data) return prev
-     return {
-      getOrder: {
-       ...prev.getOrder,
-       order: {
-        ...data.orderUpdates,
-       },
+      input: {
+        id: +params.id,
       },
-     }
     },
-   })
-  }
- }, [data, params.id, subscribeToMore])
-
- const onButtonClick = (newStatus: OrderStatus) => {
-  editOrderMutation({
-   variables: {
-    input: {
-     id: +params.id,
-     status: newStatus,
-    },
-   },
   })
- }
 
- console.log(data)
- return (
-  <div className="mt-32 container flex justify-center">
-   <Helmet>
-    <title>Order ${params.id} | Nuber Eats</title>
-   </Helmet>
-   <div className="border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center">
-    <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">Order #{params.id}</h4>
-    <h5 className="p-5 pt-10 text-3xl text-center ">${data?.getOrder.order?.total}</h5>
-    <div className="p-5 text-xl grid gap-6">
-     <div className="border-t pt-5 border-gray-700">
-      Prepared By: <span className="font-medium">{data?.getOrder.order?.restaurant?.name}</span>
-     </div>
-     <div className="border-t pt-5 border-gray-700 ">
-      Deliver To: <span className="font-medium">{data?.getOrder.order?.customer?.email}</span>
-     </div>
-     <div className="border-t border-b py-5 border-gray-700">
-      Driver: <span className="font-medium">{data?.getOrder.order?.driver?.email || 'Not yet.'}</span>
-     </div>
-     {userData?.me.role === UserRole.Client && (
-      <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Status: {data?.getOrder.order?.status}</span>
-     )}
-     {userData?.me.role === UserRole.Owner && (
-      <>
-       {data?.getOrder.order?.status === OrderStatus.Pending && (
-        <button onClick={() => onButtonClick(OrderStatus.Cooking)} className="btn">
-         Accept order
-        </button>
-       )}
-       {data?.getOrder.order?.status === OrderStatus.Cooking && (
-        <button onClick={() => onButtonClick(OrderStatus.Cooked)} className="btn">
-         Order Cooked
-        </button>
-       )}
-       {data?.getOrder.order?.status !== OrderStatus.Cooking &&
-        data?.getOrder.order?.status !== OrderStatus.Pending && (
-         <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Status: {data?.getOrder.order?.status}</span>
-        )}
-      </>
-     )}
-     {userData?.me.role === UserRole.Delivery && (
-      <>
-       {data?.getOrder.order?.status === OrderStatus.Cooked && (
-        <button onClick={() => onButtonClick(OrderStatus.PickedUp)} className="btn">
-         Picked Up
-        </button>
-       )}
-       {data?.getOrder.order?.status === OrderStatus.PickedUp && (
-        <button onClick={() => onButtonClick(OrderStatus.Delivered)} className="btn">
-         Order Delivered
-        </button>
-       )}
-      </>
-     )}
-     {data?.getOrder.order?.status === OrderStatus.Delivered && (
-      <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Thank you for using Nuber Eats.</span>
-     )}
+  console.log(data);
+
+  useEffect(() => {
+    if (data?.getOrder.ok) {
+      subscribeToMore({
+        document: ORDER_SUBSCRIPTION,
+        variables: {
+          input: {
+            id: +params.id,
+          },
+        },
+        updateQuery: (prev, { subscriptionData: { data } }: { subscriptionData: { data: orderUpdates } }) => {
+          if (!data) return prev
+          return {
+            getOrder: {
+              ...prev.getOrder,
+              order: {
+                ...data.orderUpdates,
+              },
+            },
+          }
+        },
+      })
+    }
+  }, [data, params.id, subscribeToMore])
+
+  const onButtonClick = (newStatus: OrderStatus) => {
+    editOrderMutation({
+      variables: {
+        input: {
+          id: +params.id,
+          status: newStatus,
+        },
+      },
+    })
+  }
+
+  console.log(data)
+  return (
+    <div className="mt-32 container flex justify-center">
+      <Helmet>
+        <title>Order ${params.id} | Nuber Eats</title>
+      </Helmet>
+      <div className="border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center">
+        <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">Order #{params.id}</h4>
+        <h5 className="p-5 pt-10 text-3xl text-center ">${data?.getOrder.order?.total}</h5>
+        <div className="p-5 text-xl grid gap-6">
+          <div className="border-t pt-5 border-gray-700">
+            Prepared By: <span className="font-medium">{data?.getOrder.order?.restaurant?.name}</span>
+          </div>
+          <div className="border-t pt-5 border-gray-700 ">
+            Deliver To: <span className="font-medium">{data?.getOrder.order?.customer?.email}</span>
+          </div>
+          <div className="border-t border-b py-5 border-gray-700">
+            Driver: <span className="font-medium">{data?.getOrder.order?.driver?.email || 'Not yet.'}</span>
+          </div>
+          {userData?.me.role === UserRole.Client && (
+            <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Status: {data?.getOrder.order?.status}</span>
+          )}
+          {userData?.me.role === UserRole.Owner && (
+            <>
+              {data?.getOrder.order?.status === OrderStatus.Pending && (
+                <button onClick={() => onButtonClick(OrderStatus.Cooking)} className="btn">
+                  Accept order
+                </button>
+              )}
+              {data?.getOrder.order?.status === OrderStatus.Cooking && (
+                <button onClick={() => onButtonClick(OrderStatus.Cooked)} className="btn">
+                  Order Cooked
+                </button>
+              )}
+              {data?.getOrder.order?.status !== OrderStatus.Cooking &&
+                data?.getOrder.order?.status !== OrderStatus.Pending && (
+                  <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Status: {data?.getOrder.order?.status}</span>
+                )}
+            </>
+          )}
+          {userData?.me.role === UserRole.Delivery && (
+            <>
+              {data?.getOrder.order?.status === OrderStatus.Cooked && (
+                <button onClick={() => onButtonClick(OrderStatus.PickedUp)} className="btn">
+                  Picked Up
+                </button>
+              )}
+              {data?.getOrder.order?.status === OrderStatus.PickedUp && (
+                <button onClick={() => onButtonClick(OrderStatus.Delivered)} className="btn">
+                  Order Delivered
+                </button>
+              )}
+            </>
+          )}
+          {data?.getOrder.order?.status === OrderStatus.Delivered && (
+            <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Thank you for using Nuber Eats.</span>
+          )}
+        </div>
+      </div>
     </div>
-   </div>
-  </div>
- )
+  )
 }
 
 export default Order
